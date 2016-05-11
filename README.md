cad13d4785e401ca15afdc33397202b57370cfc6
Kyle Crawshaw <kyle.crawshaw@gmail.com>
2016-05-10 22:02:24 -0400

    Update docker config. Added .env and some fixes for python3 support

--- README.md
+++ README.md
@@ -17,3 +17,23 @@ JSS_RESOURCE_BASE_URL
 JSS_USERNAME
 JSS_PASSWORD
 ```
+### Use With Docker
+Create the base vm for Docker to run on
+```
+docker-machine create -d virtualbox macdash
+```
+
+Make sure docker-machine knows which vm to use
+```
+eval $(docker-machine env macdash)
+```
+
+Build the Docker containers with docker-compose. This is based on settings in docker-compose.yml
+```
+docker-compose build
+```
+
+Start the various docker containers
+```
+docker-compose up -d
+```
--- web/app.py
+++ web/app.py
@@ -12,7 +12,7 @@ credentials = '{username}:{password}'.format(
   username=app.config['JSS_USERNAME'],
   password=app.config['JSS_PASSWORD'])
 
-authorization = 'Basic {auth}'.format(auth=b64encode(credentials))
+authorization = 'Basic {auth}'.format(auth=b64encode(credentials.encode('ascii')))
 
 def jss_request(endpoint):
     headers = {
@@ -25,7 +25,7 @@ def jss_request(endpoint):
     return r
     
 
-@application.route('/')
+@app.route('/')
 def homepage():
 
 
@@ -43,17 +43,17 @@ def homepage():
     r12 = jss_request('/computergroups/id/153')
 
 
-  return render_template('index.html', allmanaged=len(json.loads(r1.text)['computer_group']['computers']) ,
-                                       ccdmanaged=json.loads(r2.text)['computer_group']['computers'] ,
-                                       admanaged=json.loads(r3.text)['computer_group']['computers']  ,
-                                       manageddesktops=json.loads(r4.text)['computer_group']['computers'] ,
-                                       managedlaptops=json.loads(r5.text)['computer_group']['computers'] ,
-                                       manageddevices=json.loads(r6.text)['mobile_device_group']['mobile_devices'] ,
-                                       mavericksinstalled=json.loads(r7.text)['computer_group']['computers'] ,
-                                       yosemiteinstalled=json.loads(r8.text)['computer_group']['computers'] ,
-                                       elcapinstalled=json.loads(r9.text)['computer_group']['computers'] ,
-                                       lastchecktwenty=json.loads(r10.text)['computer_group']['computers'] ,
-                                       lastcheckfortyfive=json.loads(r11.text)['computer_group']['computers'] ,
-                                       lastcheckninety=json.loads(r12.text)['computer_group']['computers'])
+    return render_template('index.html', allmanaged=len(json.loads(r1.text)['computer_group']['computers']) ,
+                                         ccdmanaged=json.loads(r2.text)['computer_group']['computers'] ,
+                                         admanaged=json.loads(r3.text)['computer_group']['computers']  ,
+                                         manageddesktops=json.loads(r4.text)['computer_group']['computers'] ,
+                                         managedlaptops=json.loads(r5.text)['computer_group']['computers'] ,
+                                         manageddevices=json.loads(r6.text)['mobile_device_group']['mobile_devices'] ,
+                                         mavericksinstalled=json.loads(r7.text)['computer_group']['computers'] ,
+                                         yosemiteinstalled=json.loads(r8.text)['computer_group']['computers'] ,
+                                         elcapinstalled=json.loads(r9.text)['computer_group']['computers'] ,
+                                         lastchecktwenty=json.loads(r10.text)['computer_group']['computers'] ,
+                                         lastcheckfortyfive=json.loads(r11.text)['computer_group']['computers'] ,
+                                         lastcheckninety=json.loads(r12.text)['computer_group']['computers'])
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
