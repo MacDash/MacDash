a51ce348df19933b363439dd37ccc33c04d38eaf
Kyle Crawshaw <kyle.crawshaw@gmail.com>
2016-05-12 19:02:47 -0400

    Removed api calls in homepage endpoint. Will re-add later

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-  # run.py
+# run.py
 
 from flask import Flask, render_template
 import requests
@@ -20,40 +20,15 @@ def jss_request(endpoint):
         'accept': "application/json",
     }
     jss_request_url = '{base}{endpoint}'.format(
-        base=app.config['JSS_RESOURCE_BASE_URL'], endpoint=endpoint)
+        base=app.config['JSS_RESOURCE_BASE_URL'].strip('/'),
+        endpoint=endpoint)
     r = requests.get(jss_request_url)
     return r
-    
+
 
 @app.route('/')
 def homepage():
+    return render_template('index.html')
 
-
-    r1 = jss_request('computergroups/id/1')
-    r2 = jss_request('/computergroups/id/183')
-    r3 = jss_request('/computergroups/id/19')
-    r4 = jss_request('/computergroups/id/196')
-    r5 = jss_request('/computergroups/id/195')
-    r6 = jss_request('/mobiledevicegroups/id/5')
-    r7 = jss_request('/computergroups/id/115')
-    r8 = jss_request('/computergroups/id/134')
-    r9 = jss_request('/computergroups/id/119')
-    r10 = jss_request('/computergroups/id/136')
-    r11 = jss_request('/computergroups/id/135')
-    r12 = jss_request('/computergroups/id/153')
-
-
-    return render_template('index.html', allmanaged=len(json.loads(r1.text)['computer_group']['computers']) ,
-                                         ccdmanaged=json.loads(r2.text)['computer_group']['computers'] ,
-                                         admanaged=json.loads(r3.text)['computer_group']['computers']  ,
-                                         manageddesktops=json.loads(r4.text)['computer_group']['computers'] ,
-                                         managedlaptops=json.loads(r5.text)['computer_group']['computers'] ,
-                                         manageddevices=json.loads(r6.text)['mobile_device_group']['mobile_devices'] ,
-                                         mavericksinstalled=json.loads(r7.text)['computer_group']['computers'] ,
-                                         yosemiteinstalled=json.loads(r8.text)['computer_group']['computers'] ,
-                                         elcapinstalled=json.loads(r9.text)['computer_group']['computers'] ,
-                                         lastchecktwenty=json.loads(r10.text)['computer_group']['computers'] ,
-                                         lastcheckfortyfive=json.loads(r11.text)['computer_group']['computers'] ,
-                                         lastcheckninety=json.loads(r12.text)['computer_group']['computers'])
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
