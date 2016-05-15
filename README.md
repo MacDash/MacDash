03a8f6665142598b9ed7ceb5d4013b69a49396ee
Kyle Crawshaw <kyle.crawshaw@gmail.com>
2016-05-14 20:37:16 -0400

    Initial commit of django version of macdash

---
+++ README.md
@@ -0,0 +1,22 @@
+# MacDash
+
+## Install Requirements
+```
+pip install -r requirements.txt
+```
+
+## Create the SQLite3 Database and Migrate to 
+Create Tables(temporary)
+```
+python manage.py migrate
+```
+
+## Create a super user
+```
+python manage.py createsuperuser
+```
+
+## Run the built in server
+```
+python manage.py runserver
+```
---
+++ dash/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+# from django.shortcuts import render, redirect, get_object_or_404
+# from django.conf import settings
+# from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+
+# Create your views here.
+@login_required
+def index(request):
+    return render(request, 'index.html')
