806b334b02e995bddf6ab577cf0bb9861efccfcc
Kyle Crawshaw <kcrawshaw@edx.org>
2016-05-27 16:54:15 -0400

    Added a new list.html template. Needs more work to be more flexible

--- dash/views.py
+++ dash/views.py
@@ -5,10 +5,22 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 
+from jss.models import Computer
+
 # Create your views here.
 @login_required
 def home(request):
     return render(request, "index.html")
 
-def devices (request):
-    return render(request, "devices.html")
+@login_required
+def devices(request):
+    devices = list()
+    computers = Computer.objects.all()
+    for computer in Computer.objects.all():
+        comp_dict = computer.__dict__
+        if computer.last_contact_time_utc:
+            comp_dict['last_contact_time_utc'] = computer.last_contact_time_utc.strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            comp_dict['last_contact_time_utc'] = 'Unknown'
+        devices.append(comp_dict)
+    return render(request, "list.html", {'items': devices, 'list_title': 'Computers', 'menu_active': 'devices'})
