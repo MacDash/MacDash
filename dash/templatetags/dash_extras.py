80a1e06988b674ea129e7e6622d6bc5a7dd26629
Chris Sheppard <cshepp@fnal.gov>
2016-06-04 22:54:08 -0500

    Working on Size converter tag filter.

--- dash/templatetags/dash_extras.py
+++ dash/templatetags/dash_extras.py
@@ -17,3 +17,11 @@ def getattribute(obj, attr, default=sentinel):
         def _getattr(obj, name):
             return getattr(obj, name, default)
     return functools.reduce(_getattr, [obj]+attr.split('.'))
+
+@register.filter(name='sizeof_fmt')
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
