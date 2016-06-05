2dda67747746f503f787d3541e249363f2864111
Kyle Crawshaw <kcrawshaw@edx.org>
2016-06-05 00:01:24 -0400

    Fixed bad operand type for abs(): 'str' error

--- dash/templatetags/dash_extras.py
+++ dash/templatetags/dash_extras.py
@@ -20,6 +20,7 @@ def getattribute(obj, attr, default=sentinel):
 
 @register.filter(name='sizeof_fmt')
 def sizeof_fmt(num, suffix='B'):
+    # num = float(num)
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
