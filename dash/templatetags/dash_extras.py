646ac37d5272f2b8e89d0b8f98785f200599ec4e
Kyle Crawshaw <kcrawshaw@edx.org>
2016-06-05 19:52:27 -0400

    Added Last checkin. New templatetag for floor and dayssince

--- dash/templatetags/dash_extras.py
+++ dash/templatetags/dash_extras.py
@@ -1,5 +1,7 @@
 import functools
-from math import floor
+import math
+
+from datetime import datetime
 
 from django import template
 
@@ -9,9 +11,11 @@ register = template.Library()
 def key(d, key_name):
     return d[key_name]
 
+
 @register.filter(name='getattribute')
 def getattribute(obj, attr):
     return functools.reduce(getattr, [obj]+attr.split('.'))
+
 
 @register.filter(name='sizeof_fmt')
 def sizeof_fmt(num, suffix='B'):
@@ -22,17 +26,36 @@ def sizeof_fmt(num, suffix='B'):
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
+
 @register.filter(name='mult')
 def mult(value, arg):
     "Multiplies the arg and the value"
     return int(value) * int(arg)
+
 
 @register.filter(name='sub')
 def sub(value, arg):
     "Subtracts the arg from the value"
     return int(value) - int(arg)
 
+
 @register.filter(name='div')
 def div(value, arg):
     "Divides the value by the arg"
-    return floor(int(value) / int(arg))
+    return int(value) / int(arg)
+
+
+@register.filter(name='floor')
+def floor(value):
+    return math.floor(value)
+
+
+@register.filter(name='dayssince')
+def dayssince(date):
+    date_diff = datetime.now(tz=date.tzinfo) - date
+    if date_diff.days == 0:
+        return 'Today'
+    elif date_diff.days == 1:
+        return '{} Day'.format(date_diff.days)
+    else:
+        return '{} Days'.format(date_diff.days)
