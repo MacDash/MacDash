396564f7b0472ef7aba0a1553cc1eea5f1afe183
Kyle Crawshaw <kcrawshaw@edx.org>
2016-06-04 22:49:50 -0400

    Some refactoring of views. Added 'getattribute' template tag

--- dash/templatetags/dash_extras.py
+++ dash/templatetags/dash_extras.py
@@ -1,3 +1,5 @@
+import functools
+
 from django import template
 
 register = template.Library()
@@ -5,3 +7,13 @@ register = template.Library()
 @register.filter(name='key')
 def key(d, key_name):
     return d[key_name]
+
+sentinel = object()
+@register.filter(name='getattribute')
+def getattribute(obj, attr, default=sentinel):
+    if default is sentinel:
+        _getattr = getattr
+    else:
+        def _getattr(obj, name):
+            return getattr(obj, name, default)
+    return functools.reduce(_getattr, [obj]+attr.split('.'))
--- dash/views.py
+++ dash/views.py
@@ -7,14 +7,14 @@ from django.contrib.auth.decorators import login_required
 from jss.models import Computer, ComputerApplication
 
 
-def _format_device(computer):
-    comp_dict = computer.__dict__
-    if computer.last_contact_time_utc:
-        comp_dict['last_contact_time_utc'] = computer.last_contact_time_utc.strftime('%Y-%m-%d %H:%M:%S')
-    else:
-        comp_dict['last_contact_time_utc'] = 'Unknown'
-    comp_dict['site'] = computer.site.name
-    return comp_dict
+# def _format_device(computer):
+#     comp_dict = computer.__dict__
+#     if computer.last_contact_time_utc:
+#         comp_dict['last_contact_time_utc'] = computer.last_contact_time_utc.strftime('%Y-%m-%d %H:%M:%S')
+#     else:
+#         comp_dict['last_contact_time_utc'] = 'Unknown'
+#     comp_dict['site'] = computer.site.name
+#     return comp_dict
 
 # Create your views here.
 @login_required
@@ -24,16 +24,15 @@ def home(request):
 
 @login_required
 def devices(request):
-    devices = [_format_device(computer) for computer in Computer.objects.all()]
     context = {
-        'items': devices,
+        'items': Computer.objects.all(),
         'columns': (
             ('name', 'Name'), 
             ('asset_tag', 'Asset Tag'),
             ('mac_address', 'MAC Address'),
             ('jamf_version', 'JAMF Version'),
             ('last_contact_time_utc', 'Last Check-in'),
-            ('site', 'Site')
+            ('site.name', 'Site')
         ),
         'page_title': 'Computers',
         'menu_active': 'devices',
@@ -44,35 +43,27 @@ def devices(request):
 
 @login_required
 def applications(request):
-    applications = list()
-    for app in ComputerApplication.objects.all():
-        application = app.__dict__
-        application['install_count'] = app.computers.count()
-        applications.append(application)
-    # applications = [app.__dict__ for app in ComputerApplication.objects.all()]
     context = {
-        'items': applications,
+        'items': ComputerApplication.objects.all(),
         'columns': (
             ('name', 'Name'), 
             ('version', 'Version'),
             ('path', 'Path'),
-            ('install_count', 'Install Count')
         ),
-        'link_column': ('install_count', 'application-installed'),
+        'link_column': ('name', 'application-installed'),
         'page_title': 'Applications',
-        # 'menu_active': 'devices'
+        'menu_active': 'applications',
     }
     return render(request, "list.html", context)
 
 @login_required
 def application_installed_list(request, pk):
     application = get_object_or_404(ComputerApplication, pk=pk)
-    computers = [_format_device(computer) for computer in application.computers.all()]
     page_title = 'Computers with {name} ({version})'.format(
         name=application.name, version=application.version
     )
     context = {
-        'items': computers,
+        'items': application.computers.all(),
         'columns': (
             ('name', 'Name'), 
             ('asset_tag', 'Asset Tag'),
@@ -91,11 +82,9 @@ def application_installed_list(request, pk):
 @login_required  
 def singledevice(request, pk):
     computer = get_object_or_404(Computer, pk=pk)
-    computer_dict = _format_device(computer)
-    installed_applications = [application.__dict__ for application in computer.applications.all()]
     context = {
-        'device': computer_dict,
-        'items': installed_applications,
+        'device': computer,
+        'items': computer.applications.all(),
         'columns': (
             ('name', 'Name'), 
             ('version', 'Version'),
